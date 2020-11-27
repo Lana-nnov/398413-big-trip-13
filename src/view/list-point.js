d7c74534dd81c7e2715384c81336d81900986095
Svetlana Oshepkova <lana_nnov@mail.ru>
2020-11-28 00:31:01 +0300

    внесены правки после проверки линтеров

--- src/view/list-point.js
+++ src/view/list-point.js
@@ -10,11 +10,11 @@ const getListPoint = (point) => {
 
   const getToHouresMinutes = (ms) => {
     // const days = Math.floor(ms / (24*60*60*1000));
-    const daysms=ms % (24 * 60 * 60 * 1000);
-    const hours = Math.floor((daysms)/(60 * 60 * 1000));
-    const hoursms=ms % (60 * 60 * 1000);
-    const minutes = Math.floor((hoursms)/(60 * 1000));
-    // const minutesms=ms % (60 * 1000);
+    const daysms = ms % (24 * 60 * 60 * 1000);
+    const hours = Math.floor((daysms) / (60 * 60 * 1000));
+    const hoursms = ms % (60 * 60 * 1000);
+    const minutes = Math.floor((hoursms) / (60 * 1000));
+    // const minutesms = ms % (60 * 1000);
     return hours + `H `+ minutes + `M`;
   };
 
