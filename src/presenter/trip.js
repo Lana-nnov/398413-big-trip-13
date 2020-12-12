5c146b9fb51c93671a6e76e71d8b1488cb984204
Svetlana Oshepkova <lana_nnov@mail.ru>
2020-12-12 22:04:18 +0300

    Выношу часть связанной логики в презентеры

---
+++ src/presenter/trip.js
@@ -0,0 +1,60 @@
+import {List} from "../view/list-all";
+import Point from './point.js';
+import {render, RenderPosition, replace} from "../utils/render.js";
+import {updateItem} from "../utils/common.js";
+
+export default class Trip {
+  constructor(tripContainer, point) {
+    this._tripContainer = tripContainer;
+    this._listComponent = new List();
+    this._pointPresenter = {};
+    this._handlePointChange = this._handlePointChange.bind(this);
+    this._handleModeChange = this._handleModeChange.bind(this);
+  }
+
+  init(points) {
+    this._points = points.slice();
+    this._renderBoard();
+  }
+
+  _handlePointChange(updatedPoint) {
+    this._points = updateItem(this._points, updatedPoint);
+    this._pointPresenter[updatedPoint.id].init(updatedPoint);
+  }
+
+  _renderPoint(point) {
+    const pointPresenter = new Point(this._listComponent, this._handlePointChange, this._handleModeChange);
+    pointPresenter.init(point);   
+    this._pointPresenter[point.id] = pointPresenter;
+  }
+
+  _renderPoints() {
+    this._points.slice().forEach((point) => this._renderPoint(point));
+  }
+
+  _renderPointsList() {
+    render(this._tripContainer, this._listComponent, RenderPosition.BEFOREEND);
+    this._renderPoints();
+  }
+
+  _renderNoPoints() {
+    // Метод для рендеринга заглушки
+  }   
+
+  _handleModeChange() {
+    Object
+      .values(this._pointPresenter)
+      .forEach((presenter) => presenter.resetView());
+  }
+
+  _clearPointList() {
+    Object
+      .values(this._pointPresenter)
+      .forEach((presenter) => presenter.destroy());
+    this._pointPresenter = {};    
+  }
+
+  _renderBoard() {
+    this._renderPointsList();
+  }
+}
--- src/view/list-point.js
+++ src/view/list-point.js
@@ -64,19 +64,30 @@ class ListPoint extends AbstractView {
     this._element = null;
     this._point = point;
     this._clickHandler = this._clickHandler.bind(this);
+    this._favoriteClickHandler = this._favoriteClickHandler.bind(this);
   }
 
   getTemplate() {
     return getListPoint(this._point);
   }
 
-  _clickHandler() {
+  _clickHandler() {    
     this._callback.click();
   }
 
   setEditClickHandler(callback) {
     this._callback.click = callback;
-    this.getElement().addEventListener(`click`, this._clickHandler);
+    this.getElement().querySelector('.event__rollup-btn').addEventListener(`click`, this._clickHandler);
+  }
+  
+  _favoriteClickHandler(evt) {
+    evt.preventDefault();
+    this._callback.favoriteClick();
+  }
+  
+  setFavoriteClickHandler(callback) {
+    this._callback.favoriteClick = callback;
+    this.getElement().querySelector(`.event__favorite-btn`).addEventListener(`click`, this._favoriteClickHandler);
   }
 }
 
